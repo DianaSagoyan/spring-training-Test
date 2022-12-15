9d9bd00842e78c25ed0b681d5825aee84ff9884c
DianaSagoyan <dianasagoyan@yahoo.com>
2022-12-15 13:49:03 -0600

    Practicing Test

---
+++ spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+
+    public static String operator = "add";
+
+    public static int add(int num1, int num2){
+        return num1 + num2;
+    }
+
+}
---
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        System.out.println("Add method");
+        int actual = Calculator.add(2, 3);
+        assertEquals(5, actual, "Test failed");
+    }
+
+    @Test
+    void testCase1() {
+//        System.out.println("Test Case 1");
+        fail("Not implemented yet");
+    }
+
+    @Test
+    void testCase2() {
+        System.out.println("Test Case 2");
+        assertTrue(Calculator.operator.equals("add"));
+        assertEquals("add", Calculator.operator);
+    }
+
+    @Test
+    void testCase3() {
+        System.out.println("Test Case 3");
+        assertArrayEquals(new int[]{1, 2, 3}, new int[]{1, 3, 2}, "Arrays are not same.");
+    }
+
+    @Test
+    void testCase4() {
+        System.out.println("Test Case 4");
+
+        String nullString = null;
+        String notNullString = "Cydeo";
+
+        assertNull(nullString);
+        assertNotNull(notNullString);
+
+//        assertNull(notNullString);
+        assertNotNull(nullString);
+    }
+
+    @Test
+    void testCase5() {
+        System.out.println("Test Case 5");
+    }
+}
