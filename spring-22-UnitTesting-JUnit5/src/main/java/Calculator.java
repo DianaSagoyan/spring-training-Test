99ea4e19140f274523a9677ba8ac1ba3448a6652
DianaSagoyan <dianasagoyan@yahoo.com>
2022-12-18 19:32:12 -0600

    Practicing Unit tests

--- spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
+++ spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
@@ -6,4 +6,11 @@ public class Calculator {
         return num1 + num2;
     }
 
+    public static int add2(int num1, int num2){
+        if(num1 > num2){
+            throw new IllegalArgumentException("My exception");
+        }
+        return num1 + num2;
+    }
+
 }
--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -1,14 +1,44 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.nio.file.AccessDeniedException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+    @BeforeAll
+    static void setUpAll(){
+        System.out.println("BeforeAll is executed");
+    }
+
+    @AfterAll
+    static void tearDownAll(){
+        System.out.println("AfterAll is executed");
+    }
+
+    @BeforeEach
+    void setUpEach(){
+        System.out.println("BeforeEach is executed");
+    }
+
+    @AfterEach
+    void tearDownEach(){
+        System.out.println("AfterEach is executed");
+    }
 
     @Test
     void add() {
         System.out.println("Add method");
         int actual = Calculator.add(2, 3);
         assertEquals(5, actual, "Test failed");
+    }
+
+    @Test
+    void add2(){
+        System.out.println("Add2 method");
+//        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(3, 2));
+//        assertThrows(AccessDeniedException.class, () -> Calculator.add2(3, 2));
+        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(2, 3));
     }
 
     @Test
@@ -27,7 +57,8 @@ class CalculatorTest {
     @Test
     void testCase3() {
         System.out.println("Test Case 3");
-        assertArrayEquals(new int[]{1, 2, 3}, new int[]{1, 3, 2}, "Arrays are not same.");
+        assertArrayEquals(new int[]{1, 2, 3}, new int[] {1, 2, 3}, "Arrays are not same");
+
     }
 
     @Test
@@ -47,5 +78,12 @@ class CalculatorTest {
     @Test
     void testCase5() {
         System.out.println("Test Case 5");
+        Calculator c1 = new Calculator();
+        Calculator c2 = c1;
+        Calculator c3 = new Calculator();
+
+        assertSame(c1, c2);
+        assertNotSame(c1, c3);
+
     }
 }
